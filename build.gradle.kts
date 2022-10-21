3cb947705fe637c3058a1bb5e313a4adf26d2684
Sergey Shatunov <me@aur.rocks>
2022-10-22 05:16:55 +0800

    Reproducer for #22440

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+        classpath("org.jetbrains.kotlinx:atomicfu-gradle-plugin:0.18.4")
+    }
+}
